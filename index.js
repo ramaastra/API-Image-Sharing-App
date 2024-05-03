08921dc04f0711e4434919562020cdc47d7d7c50
Rama Astra <ramaastra333@gmail.com>
2024-05-03 17:59:48 +0800

    refactor: create a separate handler for server and client error

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./router');
+const handler = require('./handler');
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(morgan('dev'));
 
 app.use('/api/v1', router);
+app.use(handler.serverError);
+app.use(handler.clientError);
 
 app.get('/', (req, res) => {
   res.status(200).json({
     status: true,
     message: 'API is available at /api/v1',
-    data: null
-  });
-});
-
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({
-    status: false,
-    message: err.message,
-    data: null
-  });
-});
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    status: false,
-    message: `${req.method} ${req.url} is not available`,
     data: null
   });
 });
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -5,9 +5,11 @@ const ALLOWED_MIME_TYPES = ['image/jpeg', 'image/png'];
 const upload = multer({
   fileFilter: (req, file, callback) => {
     if (!ALLOWED_MIME_TYPES.includes(file.mimetype)) {
-      const error = new Error(
-        `Only accepts ${ALLOWED_MIME_TYPES.join(', ')} file mime type`
-      );
+      const error = new multer.MulterError();
+      error.code = 'INVALID_FILE_TYPE';
+      error.message = `Only accepts ${ALLOWED_MIME_TYPES.join(
+        ', '
+      )} file mime type`;
       callback(error, false);
     }
 
